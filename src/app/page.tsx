62fc437fc30b62de5051fe3424dc4aed9e71e4df
Cristian Prodius <114479945+CristianProdius@users.noreply.github.com>
2025-06-03 09:57:47 +0300

    stats added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import CourseRevenueCalculator from "@/components/LandingPage/calculator";
 import { CardsForLinking } from "@/components/LandingPage/card";
 import { Hero } from "@/components/LandingPage/hero";
+import PremiumStatsComponent from "@/components/LandingPage/PremiumStatsComponent";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <CardsForLinking />
+      <PremiumStatsComponent />
       <CourseRevenueCalculator />
     </>
   );
