b3e7416a56ae3433a513f4994922cf752bbae9c3
Cristian Prodius <114479945+CristianProdius@users.noreply.github.com>
2025-06-04 20:04:54 +0300

    nav removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,10 @@ import CourseRevenueCalculator from "@/components/LandingPage/calculator";
 import { CardsForLinking } from "@/components/LandingPage/card";
 import { Hero } from "@/components/LandingPage/hero";
 import PremiumStatsComponent from "@/components/LandingPage/PremiumStatsComponent";
-import { Nav } from "@/components/navbar";
 
 export default function Home() {
   return (
     <>
-      <Nav />
       <Hero />
       <CardsForLinking />
       <PremiumStatsComponent />
